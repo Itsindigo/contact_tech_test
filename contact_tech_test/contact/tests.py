78c582a7ce53f48bb7a2a8f1d852dbfc8eea2e7c
Matthew Bridges <mattbridgesbusiness@gmail.com>
2016-06-20 19:29:36 +0100

    Test valid and invalid email

--- contact_tech_test/contact/tests.py
+++ contact_tech_test/contact/tests.py
@@ -18,8 +18,14 @@ class ContactTest(TestCase):
         response = contact_new(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_form_raises_error_with_invalid_email(self):
+    def test_form_accepts_valid_email_and_name(self):
         form_data = {'contact_name': 'something',
                      'contact_email': 'test@test.com'}
         form = ContactForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+    def test_form_raises_error_with_invalid_email(self):
+        form_data = {'contact_name': 'something',
+                     'contact_email': 'not-an-email'}
+        form = ContactForm(data=form_data)
+        self.assertFalse(form.is_valid())
