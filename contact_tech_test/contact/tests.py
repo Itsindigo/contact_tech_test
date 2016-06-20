effc0a8cf02b97957633eed6644cd343ba9b20b8
Matthew Bridges <mattbridgesbusiness@gmail.com>
2016-06-20 16:09:40 +0100

    First tests passing Now contact renders hello world unit and function test

--- contact_tech_test/contact/tests.py
+++ contact_tech_test/contact/tests.py
@@ -1,4 +1,21 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+
+from contact.views import index
+
+class IndexTest(TestCase):
+
+    def test_contact_url_resolves_to_index_view(self):
+        found = resolve('/contact/')
+        self.assertEqual(found.func, index)
+
+    def test_index_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    
+
 # Create your tests here.
