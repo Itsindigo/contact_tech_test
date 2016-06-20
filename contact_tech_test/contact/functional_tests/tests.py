130192f4bb073f1425c858b1ff77da96cc00c675
Matthew Bridges <mattbridgesbusiness@gmail.com>
2016-06-20 15:54:44 +0100

    Scaffolding Project initialised with contact application, first functional failing test written and webdriver setup.

---
+++ contact_tech_test/contact/functional_tests/tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_retrieve_contact_form(self):
+        self.browser.get('http://localhost:8000/contact/')
+        self.assertIn('Contact form', self.browser.title)
+
+        self.fail('Finish the test!')
---
+++ contact_tech_test/contact/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+# Create your tests here.
