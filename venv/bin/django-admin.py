8f11cde86ace4c259f22aa2dbf36e387ac247f65
Matthew Bridges <mattbridgesbusiness@gmail.com>
2016-06-25 14:03:14 +0100

    heroku deploy

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matthewbridges/Projects/python/python-ing/matt-bridges-tech-test/contact_tech_test/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
